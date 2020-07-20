5e69becbc5ad9978471d0f24f3084d4250d7402a
Alex Wander <alexcwander@gmail.com>
2020-07-20 11:42:21 -0500

    update db connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+// heroku db url
+$DATABASE_URL=parse_url('postgres://srzgnylnpcjikt:61ff2ee11c7960b3e99ad3642729c136fce567f470ed41e9fadf44de250dc7cb@ec2-52-1-95-247.compute-1.amazonaws.com:5432/dappp5qehq7ar');
+
 return [
 
     /*
@@ -65,12 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
