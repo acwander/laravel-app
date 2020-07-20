6c22e8f24e6a68a4763f9cbcee10cf42ffee081f
Alex Wander <alexcwander@gmail.com>
2020-07-20 13:11:24 -0500

    added urls to ignore token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'https://player-scorecard.herokuapp.com/register',
+        'https://player-scorecard.herokuapp.com/login',
+
     ];
 }
