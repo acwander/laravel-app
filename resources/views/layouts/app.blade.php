8a4b7df1807e7865016a41a21cd541b9d72ded17
Alex Wander <alexcwander@gmail.com>
2020-07-20 20:16:50 -0500

    changed link tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>Player Scorecard | Login</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     <script src="js/app.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
